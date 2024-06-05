c084a3d418202f7690542a25aac06229be37da80
Lucas <lucascteixeira0gmail.com>
2024-06-05 20:28:37 -0300

    initial commit

---
+++ src/components/Phone.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+import { cn } from '@/lib/utils'
+import { HTMLAttributes } from 'react'
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string
+  dark?: boolean
+}
+
+const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
+  return (
+    <div
+      className={cn(
+        'relative pointer-events-none z-50 overflow-hidden',
+        className,
+      )}
+      {...props}
+    >
+      <img
+        src={
+          dark
+            ? '/phone-template-dark-edges.png'
+            : '/phone-template-white-edges.png'
+        }
+        alt=""
+        className="pointer-events-none z-50 select-none"
+      />
+
+      <div className="absolute -z-10 inset-0">
+        <img src={imgSrc} className="object-cover" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Phone
