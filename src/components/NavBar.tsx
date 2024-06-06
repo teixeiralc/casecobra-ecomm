23f13c8c6b25bb6b875ed0fc2fd9bc0c1932997e
Lucas <lucascteixeira0@gmail.com>
2024-06-05 21:06:36 -0300

    feat: add nav

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,92 @@
+import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
+import MaxWidthWrappper from './MaxWidthWrapper'
+import { buttonVariants } from './ui/button'
+
+const NavBar = () => {
+  const user = undefined
+  const isAdmin = undefined
+
+  return (
+    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrappper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="z-40 font-semibold text-lg">
+            case<span className="text-green-600">cobra</span>
+          </Link>
+
+          <div className="h-full flex items-center space-x-4">
+            {user ? (
+              <>
+                <Link
+                  href="/api/auth/logout"
+                  className={buttonVariants({
+                    size: 'default',
+                    variant: 'ghost',
+                  })}
+                >
+                  Sign out
+                </Link>
+                {isAdmin ? (
+                  <Link
+                    href="/api/auth/logout"
+                    className={buttonVariants({
+                      size: 'default',
+                      variant: 'ghost',
+                    })}
+                  >
+                    Dashboard ✨
+                  </Link>
+                ) : null}
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: 'default',
+                    className: 'hidden sm:flex items-center gap-1 text-white',
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/api/auth/register"
+                  className={buttonVariants({
+                    size: 'default',
+                    variant: 'ghost',
+                  })}
+                >
+                  Sign up
+                </Link>
+                <Link
+                  href="/api/auth/login"
+                  className={buttonVariants({
+                    size: 'default',
+                    variant: 'ghost',
+                  })}
+                >
+                  Login
+                </Link>
+                <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: 'default',
+                    className: 'hidden sm:flex items-center gap-1 text-white',
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrappper>
+    </nav>
+  )
+}
+
+export default NavBar
