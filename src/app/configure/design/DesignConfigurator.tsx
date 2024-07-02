eb0cdab278825b3a2f88df01ef61471cd04e67cb
Lucas <lucascteixeira0@gmail.com>
2024-07-02 19:56:05 -0300

    feat: add colors to case configurator

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -2,8 +2,13 @@
 
 import ResizeHandle from '@/components/ResizeHandle'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
+import { Label } from '@/components/ui/label'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { cn } from '@/lib/utils'
+import { COLORS } from '@/validators/option-validator'
+import { RadioGroup } from '@headlessui/react'
 import NextImage from 'next/image'
+import { useState } from 'react'
 import { Rnd } from 'react-rnd'
 
 interface DesignConfiguratorProps {
@@ -17,6 +22,10 @@ const DesignConfigurator = ({
   imgUrl,
   imageDimensions,
 }: DesignConfiguratorProps) => {
+  const [options, setOptions] = useState<{ color: (typeof COLORS)[number] }>({
+    color: COLORS[0],
+  })
+
   return (
     <div className="relative mb-20 mt-20 grid grid-cols-3 pb-20">
       <div className="focus: relative col-span-2 flex h-[37.5rem] w-full max-w-4xl items-center justify-center overflow-hidden rounded-lg border-2 border-dashed border-gray-300 p-12 text-center ring-offset-2 focus:outline-none focus:ring-2 focus:ring-primary">
@@ -36,7 +45,7 @@ const DesignConfigurator = ({
           <div
             className={cn(
               'absolute inset-0 bottom-px left-[3px] right-[3px] top-px rounded-[32px]',
-              `bg-blue-900`,
+              `bg-${options.color.tw}`,
             )}
           />
         </div>
@@ -66,6 +75,55 @@ const DesignConfigurator = ({
             />
           </div>
         </Rnd>
+      </div>
+
+      <div className="flex h-[37.5rem] flex-col bg-white">
+        <ScrollArea className="relative flex-1 overflow-auto">
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 bottom-0 z-10 h-12 bg-gradient-to-t from-white"
+          />
+          <div className="px-8 pb-12 pt-8">
+            <h2 className="text-3xl font-bold tracking-tight">
+              Customize your case
+            </h2>
+            <div className="my-6 h-px w-full bg-zinc-300" />
+
+            <div className="relative mt-4 flex h-full flex-col justify-between">
+              <RadioGroup
+                value={options.color}
+                onChange={(val) => {
+                  setOptions((prev) => ({ ...prev, color: val }))
+                }}
+              >
+                <Label>Color: {options.color.label}</Label>
+                <div className="mt-3 flex items-center space-x-3">
+                  {COLORS.map((color) => (
+                    <RadioGroup.Option
+                      key={color.value}
+                      value={color}
+                      className={({ active, checked }) =>
+                        cn(
+                          'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full border-2 border-transparent p-0.5 focus:outline-none focus:ring-0 active:outline-none active:ring-0',
+                          {
+                            [`border-${color.tw}`]: active || checked,
+                          },
+                        )
+                      }
+                    >
+                      <span
+                        className={cn(
+                          `bg-${color.tw}`,
+                          'h-8 w-8 rounded-full border border-black border-opacity-10',
+                        )}
+                      />
+                    </RadioGroup.Option>
+                  ))}
+                </div>
+              </RadioGroup>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   )
---
+++ src/validators/option-validator.ts
@@ -0,0 +1,33 @@
+// bg-zinc-900 border-zinc-900
+// bg-blue-950 border-blue-950
+// bg-rose-950 border-rose-950
+// bg-orange-500 border-orange-500
+// bg-green-500 border-green-500
+
+export const COLORS = [
+  {
+    label: 'Black',
+    value: 'black',
+    tw: 'zinc-900',
+  },
+  {
+    label: 'Blue',
+    value: 'blue',
+    tw: 'blue-950',
+  },
+  {
+    label: 'Rose',
+    value: 'rose',
+    tw: 'rose-950',
+  },
+  {
+    label: 'Orange',
+    value: 'orange',
+    tw: 'orange-500',
+  },
+  {
+    label: 'Green',
+    value: 'green',
+    tw: 'green-500',
+  },
+] as const
